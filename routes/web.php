ee59cdafa85821995804f0ba616aeb5b2a4ab1d7
elvilo <gabriel.viloria0812@gmail.com>
2019-08-11 10:40:25 -0400

    creamos la ruta

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/usuarios', function(){
+ return "eres un usuario";
+});
+Route::get('/usuarios/{id}',function($id){
+    return "eres un usuario y tu id es:  $id";
+})->where('id','[0-9]+');
+Route::get('usuarios/nombre',function(){
+return "usuario crear";
+});
+Route::get('/usuarios/{nombre}/{nickname?}',function($nombre,$nickname=null){
+if($nickname){
+return "eres el usuario $nombre y tu apodo es: $nickname";
+}else{
+return "eres el usuario $nombre y no tienes nickname";
+}
 });
